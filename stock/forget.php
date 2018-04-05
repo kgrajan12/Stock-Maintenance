7a002ebe78c578aa2604df3d3760c5d9f61b5ed3
Gajarajan <kgrajan.12@gmail.com>
2018-04-05 17:50:12 +0530

    adding forget.php for recover password

---
+++ stock/forget.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forget Password</title>
+	<link rel="stylesheet" type="text/css" href="product/pro/sm.css">
+</head>
+<body>
+	<h1>Forget Password</h1>
+	<div id="aform">
+		<form method="POST" action="aforget.php">
+			<p>
+				<label>Username :</label>
+				<input type="text" name="un">
+			</p><p style="margin-left: 100px;">
+				<input type="submit" id="submit" name="sub" value="Submit">
+				<input type="submit" id="submit" name="sub" value="Cancel">
+			</p>
+		</form>
+	</div>
+</body>
+</html>
