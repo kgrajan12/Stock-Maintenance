f41e2868d12cba705b0bc075adb561db66640b2e
Gajarajan <kgrajan.12@gmail.com>
2018-06-09 06:56:58 +0530

    adding remove.php

---
+++ stock/remove.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Remove Item</title>
+	<link rel="stylesheet" type="text/css" href="product/pro/sm.css">
+</head>
+<body>
+	<div id="aform">
+	<form method="POST" action="removed.php">
+		<p>
+			<label>Enter the Sl.No to remove</label><br>
+			<input type="text" name="del">
+		</p>
+		<p>
+			<input id="submit" type="submit" name="sub" value="Remove">
+			<input id="submit" type="submit" name="sub" value="Cancel">
+		</p>
+	</form></div>
+</body>
+</html>
