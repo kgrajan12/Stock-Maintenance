64b643885d0e831d4bd11cf7761e30da2574a001
Gajarajan <kgrajan.12@gmail.com>
2018-06-25 23:50:37 +0530

    adding rmnot.php

---
+++ stock/rmnot.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Remove Notification</title>
+	<link rel="stylesheet" type="text/css" href="product/pro/sm.css">
+</head>
+<body>
+	<h1>Remove Notification</h1>
+	<div id="aform">
+		<form method="POST" action="armnot.php">
+			<p>
+				<label>which one is want to be remove</label><br>
+				<input type="text" name="un" style="margin-left: 30%;width: 67%;">
+			</p><p style="margin-left: 47%;">
+				<input type="submit" id="submit" value="Submit" name="sub">
+				<input type="submit" id="submit" value="Cancel" name="sub">
+			</p>
+		</form>
+	</div>
+</body>
+</html>
